33ae3eea219d0ceee3bfed4ad9e3d0edfaaa5771
juhengmo <36252882+juhengmo@users.noreply.github.com>
2018-05-10 23:58:28 -0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -122,7 +122,7 @@ int Graph:: DFS(int startNode) //return the number of nodes visited using BFS st
 // if there are multiple nodes with the same DFS run length, return the smallest node
 int Graph:: bestStartVertex()
 {
-	int length = new int[countNodes];
+	length = new int[countNodes];
 	int highest = 0;
 	int longest = 0;
 	for(int i=0;i< countNodes;i++){
