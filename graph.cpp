9436385ee9fe9dd2bcaee82c63c47df1f75a29e4
juhengmo <36252882+juhengmo@users.noreply.github.com>
2018-05-11 15:35:18 -0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -119,47 +119,21 @@ void Graph:: readData(string fileName) // reads data from a specified file
 }
 int Graph:: DFS(int startNode) //return the number of nodes visited using BFS starting at startNode and accumulating values at each node, as long as the budget remains positive
 {
-	 int n=1;
-
-    	int v=startNode;
-
-    	int k=1;
-
-    
-
-    	for(int j=nodes;j>=0;j--)
-
-    {
-
-        if(arr[v][j]!=0)
-
-        {
-
-            //    stk[top]=j;
-
-            if(budget>0)
-
-            {
-
-                
-
-                amount=amount-load[v];
-
-                n++;
-
-                v=j;
-
-            }
-
-        }
-
-        
-
-        //v=stk[--top];
-
-    }
-
-    return n;
+	if(amount > 0){
+		amount = amount + load[startNode];
+		visited[startNode] = true;
+		for(int i = 0;i<countNode;i++){
+			if(array[startNode][i] == 1){
+				if(visited[i] == false){
+					n++;
+					return DFS(i);
+				}
+			}
+		}
+	}
+	else{
+		return n;
+	}
 }
 
 // return the starting node that gives a longest DFS run before running out of budget
